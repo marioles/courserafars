c17952d1312d82eab04a4541904f7ec0c6f647bc
marioles <67806174+marioles@users.noreply.github.com>
2020-07-04 17:54:52 -0400

    Initial commit

---
+++ R/courserafars.R
@@ -0,0 +1,10 @@
+#' Coursera Fars Package
+#'
+#' This package is the final assignment of the course.
+#'
+#' @docType package
+#'
+#' @author Mario Morales \email{mario@moralesalfaro.cl}
+#'
+#' @name courserafars
+NULL
---
+++ tests/test_that.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(courserafars)
+
+expect_that(make_filename(2012), equals("accident_2012.csv.bz2"))
+
